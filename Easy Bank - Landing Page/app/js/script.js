1f328471d3544b6df5f661cf58f8a198021e9257
Wanderson Silva <wandersonsilva768@gmail.com>
2021-01-21 01:57:04 -0300

    feat: criada a animação do menu hamburger

---
+++ Easy Bank - Landing Page/app/js/script.js
@@ -0,0 +1,12 @@
+(function () {
+
+
+  const btnHamburger = document.querySelector('#btnHamburger');
+
+
+  btnHamburger.addEventListener('click', e => {
+    btnHamburger.classList.toggle('open');
+  })
+
+
+})();
